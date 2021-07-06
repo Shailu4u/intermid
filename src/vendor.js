e65a6c84023502e3c6267821ec22bbd4bd422427
Shailendra Kumar <shailu.snist@gmail.com>
2021-07-06 18:13:32 +0530

    Refactored code.

--- src/vendor.js
+++
@@ -1,6 +0,0 @@
-import "./assets/vendor/aos/aos.js";
-import "./assets/vendor/bootstrap/js/bootstrap.bundle.min.js";
-import "./assets/vendor/glightbox/js/glightbox.min.js";
-import "./assets/vendor/php-email-form/validate.js";
-import "./assets/vendor/purecounter/purecounter.js";
-import "./assets/vendor/swiper/swiper-bundle.min.js";
